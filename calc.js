2dd5a6271e970f74433a8dbf51f472ebf0f7a225
Gemma <gemma-owen2011@hotmail.co.uk>
2022-07-03 20:02:04 +0100

    Other buttons connected to display function to update calculator display area. Multply button updated to display the more conventional display and javascript updated to replace these instances before the equation is evaluated.

--- calc.js
+++ calc.js
@@ -5,3 +5,9 @@ function clearScreen(){
 function display(value) {
 	document.getElementById("result").value += value;
 }
+
+function calculate() {
+	var sum = document.getElementById("result").value.replace(/X/g, "*");
+	var output = eval(sum);
+	document.getElementById("result").value= output;
+}
